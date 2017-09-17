120dc752d9df78808db9e6bb683cb6ca36f7582e
hafizmabuzar <hafizmabuzar@hotmail.com>
2017-09-17 16:13:24 +0500

    socket work

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('socket.io')();
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+app.io = io;
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { serverMsg: 'Welcome to chat window' });
+  socket.on('userSays', function (data) {
+    socket.emit('userMsg', { msg: data.userMsg});
+    socket.broadcast.emit('userMsg', { msg: data.userMsg});
+    console.log(data);
+  });
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +36,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
