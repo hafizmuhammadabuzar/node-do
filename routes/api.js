dabc1d41081a6ad8893203ee4cc96d94fcf9f65c
hafizmabuzar <hafizmabuzar@hotmail.com>
2017-09-27 12:09:17 +0500

    icons add

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('./../db_connect');
+var validator = require('express-validator');
+router.use(validator());
 
 /* GET home page. */
 var returnRouter = function(db) {
+var sql;
 
   router.get('/companiesConversions', function(req, res) {
-    var sql = "select company, conversion from company_conversions";
+    sql = "select company, conversion from company_conversions";
     db.query(sql, function (err, companies) {
-      if (err) throw err;
-
-      console.log(companies);
-
-      
+      if (err) throw err;      
       var company = '';
       var result = [];
       
@@ -32,6 +30,27 @@ var returnRouter = function(db) {
         company = value.company;
       })
 
+      res.json(result);
+    });
+  });
+
+  router.get('/getHistory/:type', function(req, res, next){
+
+    req.checkQuery('company', 'Company name required').notEmpty();
+    req.checkQuery('conversion', 'Conversion required').notEmpty();
+
+    var v_errors = req.validationErrors();
+    if(v_errors){
+      res.json(v_errors);
+    }
+    var table = req.params.type+"_rates";
+    sql = "select time, close, high, low, open from "+table+" where company='"+req.query.company+"' and conversion='"+req.query.conversion+"'";
+    var query = db.query(sql, function(err, data){
+      var result = {
+        status : 'Success',
+        msg : 'Daily History',
+        data: data
+      }
       res.json(result);
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var db = require('./../db_connect');
-var fs = require('fs');
+var async = require('async');
+var promise = require('promise');
 
 /* GET home page. */
 var returnRouter = function(io) {
@@ -23,42 +24,119 @@ var returnRouter = function(io) {
 
   router.get('/daily/rates', function(req, res) {
     
-    var dailyData = [];
+    var myObject = [];
     var sql = "select company, conversion from company_conversions";
     db.query(sql, function (err, companies) {
       if (err) throw err;
+      
+      // companies.forEach(function(cmp, index){
+      //   var conv = cmp.conversion.split('/');
+      //   const timestamp = Math.floor(new Date() / 1000);
+      //   const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
 
-      companies.forEach(function(cmp){
-          
-          var conv = cmp.conversion.split('/');
-          const timestamp = Math.floor(new Date() / 1000);
-          const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
-          
-          request.get({ url: link }, function(error, response, body) { 
+      //   myObject.push({'link': link, 'company': cmp.company, 'conversion': cmp.conversion});
+      // });
+      
+    myObject.push({'link': "https://min-api.cryptocompare.com/data/histoday?fsym=ZEC&tsym=USD&limit=2000&toTs=1505991061&e=Kraken", 'company': "Kraken", 'conversion': "ZEC/USD"});
+
+    Promise.all([myObject]).then(function() {
+
+      var dailyData = [];
+
+      function getDailyData(myObject){
+
+        var count = myObject.length;
+        var values;
+        if(count > 0){
+
+          request.get({ url: myObject[count-1].link }, function(error, response, body) {
             if (!error && response.statusCode == 200) { 
-              
               var data = JSON.parse(body);
-              data = data.Data[data.length-1];
-
-              if(Array.isArray(data) && data.length > 0){
-                if(val.close != 0){
-                  var values = [data.time, val.close, val.high, val.low, val.open, val.volumefrom, val.volumeto, cmp.company, cmp.conversion];
-                  dailyData.push(values);
-                }
+              data = data.Data;
+              var length = data.length;
+              
+              if(Array.isArray(data) && length > 0 && data[length-1].close != 0){
+                values = [data[length-1].time, data[length-1].close, data[length-1].high, data[length-1].low, data[length-1].open, data[length-1].volumefrom, data[length-1].volumeto, myObject[count-1].company, myObject[count-1].conversion];
+                
+                dailyData.push(values);
+                console.log(dailyData); 
+                var popedObj = myObject.slice(0, -1);
+                return getDailyData(popedObj);
               }
             }
           });
-        });
-      });
+          
+        }
+        else{
+          return 'End';
+        }
+      }
 
-      var sql = "insert into daily_rates (time, close, open, high, low, volumefrom, volumeto, company, conversion) values ?";
-      var query = db.query(sql, [dailyData], function (err, response) {
-        if (err) throw err;
-        console.log(cmp.conversion);
-      });
+      getDailyData(myObject);
 
-    res.json({msg: 'Saved'});
+    }).then(function(){
+      console.log('End of promise');
+
+    });
+
   });
+
+    
+    // var sql = "select company, conversion from company_conversions";
+    // db.query(sql, function (err, companies) {
+    //   if (err) throw err;
+      
+    //   var dailyData = [];
+    //   async.forEach(companies, function(cmp, callback){
+    //     var conv = cmp.conversion.split('/');
+    //     const timestamp = Math.floor(new Date() / 1000);
+    //     const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
+
+    //     var dataObject = {
+    //       'link': link,
+    //       'company': cmp.company,
+    //       'conversion': cmp.conversion
+    //     }
+
+        // getDailyData(dataObject, async function(reqBack){
+        //   dailyData.push(reqBack);
+        // });
+
+    //     console.log('First loop');
+    //     callback();
+    //   }, function (err) {
+    //     if (err) { throw err; }
+    //     console.log('Done');
+    //   });
+    // });
+
+
+      // var sql = "insert into daily_rates (time, close, open, high, low, volumefrom, volumeto, company, conversion) values ?";
+      // var query = db.query(sql, [dailyData], function (err, response) {
+      //   if (err) throw err;
+      //   console.log(cmp.conversion);
+      // });
+
+    // res.json({msg: 'Saved'});
+  });
+
+
+  function oldcleargetDailyData(myObject, callback){
+
+    request.get({ url: myObject.link }, function(error, response, body) { 
+      if (!error && response.statusCode == 200) { 
+        var data = JSON.parse(body);
+        data = data.Data;
+        var length = data.length;
+        
+        if(Array.isArray(data) && length > 0 && data[length-1].close != 0){
+           var values = [data[length-1].time, data[length-1].close, data[length-1].high, data[length-1].low, data[length-1].open, data[length-1].volumefrom, data[length-1].volumeto, myObject.company, myObject.conversion];
+           callback(values);
+        }
+      }
+    });
+    
+  }
 
   return router;
 }
