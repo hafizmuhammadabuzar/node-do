9141081449e0bb32ddd19cba51bbe031194f2697
hafizmabuzar <hafizmabuzar@hotmail.com>
2017-09-27 16:38:53 +0500

    company conversion update

--- routes/index.js
+++ routes/index.js
@@ -29,87 +29,24 @@ var returnRouter = function(io) {
     db.query(sql, function (err, companies) {
       if (err) throw err;
       
-      // companies.forEach(function(cmp, index){
-      //   var conv = cmp.conversion.split('/');
-      //   const timestamp = Math.floor(new Date() / 1000);
-      //   const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
+      companies.forEach(function(cmp, index){
+        var conv = cmp.conversion.split('/');
+        const timestamp = Math.floor(new Date() / 1000);
+        const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
 
-      //   myObject.push({'link': link, 'company': cmp.company, 'conversion': cmp.conversion});
-      // });
-      
-    myObject.push({'link': "https://min-api.cryptocompare.com/data/histoday?fsym=ZEC&tsym=USD&limit=2000&toTs=1505991061&e=Kraken", 'company': "Kraken", 'conversion': "ZEC/USD"});
-
-    Promise.all([myObject]).then(function() {
-
-      var dailyData = [];
-
-      function getDailyData(myObject){
-
-        var count = myObject.length;
-        var values;
-        if(count > 0){
-
-          request.get({ url: myObject[count-1].link }, function(error, response, body) {
-            if (!error && response.statusCode == 200) { 
-              var data = JSON.parse(body);
-              data = data.Data;
-              var length = data.length;
-              
-              if(Array.isArray(data) && length > 0 && data[length-1].close != 0){
-                values = [data[length-1].time, data[length-1].close, data[length-1].high, data[length-1].low, data[length-1].open, data[length-1].volumefrom, data[length-1].volumeto, myObject[count-1].company, myObject[count-1].conversion];
-                
-                dailyData.push(values);
-                console.log(dailyData); 
-                var popedObj = myObject.slice(0, -1);
-                return getDailyData(popedObj);
-              }
+        request.get({ url: link }, function(error, response, body) { 
+          if (!error && response.statusCode == 200) { 
+            var data = JSON.parse(body);
+            data = data.Data;
+            var length = data.length;
+            
+            if(Array.isArray(data) && length > 0 && data[length-1].close != 0){
+               var values = [data[length-1].time, data[length-1].close, data[length-1].high, data[length-1].low, data[length-1].open, data[length-1].volumefrom, data[length-1].volumeto, cmp.company, cmp.conversion];
             }
-          });
-          
-        }
-        else{
-          return 'End';
-        }
-      }
-
-      getDailyData(myObject);
-
-    }).then(function(){
-      console.log('End of promise');
-
-    });
-
+          }
+        });
+      });
   });
-
-    
-    // var sql = "select company, conversion from company_conversions";
-    // db.query(sql, function (err, companies) {
-    //   if (err) throw err;
-      
-    //   var dailyData = [];
-    //   async.forEach(companies, function(cmp, callback){
-    //     var conv = cmp.conversion.split('/');
-    //     const timestamp = Math.floor(new Date() / 1000);
-    //     const link = "https://min-api.cryptocompare.com/data/histoday?fsym="+conv[0]+"&tsym="+conv[1]+"&limit=2&toTs="+timestamp+"&e="+cmp.company;
-
-    //     var dataObject = {
-    //       'link': link,
-    //       'company': cmp.company,
-    //       'conversion': cmp.conversion
-    //     }
-
-        // getDailyData(dataObject, async function(reqBack){
-        //   dailyData.push(reqBack);
-        // });
-
-    //     console.log('First loop');
-    //     callback();
-    //   }, function (err) {
-    //     if (err) { throw err; }
-    //     console.log('Done');
-    //   });
-    // });
-
 
       // var sql = "insert into daily_rates (time, close, open, high, low, volumefrom, volumeto, company, conversion) values ?";
       // var query = db.query(sql, [dailyData], function (err, response) {
@@ -119,24 +56,6 @@ var returnRouter = function(io) {
 
     // res.json({msg: 'Saved'});
   });
-
-
-  function oldcleargetDailyData(myObject, callback){
-
-    request.get({ url: myObject.link }, function(error, response, body) { 
-      if (!error && response.statusCode == 200) { 
-        var data = JSON.parse(body);
-        data = data.Data;
-        var length = data.length;
-        
-        if(Array.isArray(data) && length > 0 && data[length-1].close != 0){
-           var values = [data[length-1].time, data[length-1].close, data[length-1].high, data[length-1].low, data[length-1].open, data[length-1].volumefrom, data[length-1].volumeto, myObject.company, myObject.conversion];
-           callback(values);
-        }
-      }
-    });
-    
-  }
 
   return router;
 }
